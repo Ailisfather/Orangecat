b214d771c5cd97b1a51bd9596fb76f090f4404ac
Ailisfather <yimingw@smu.edu>
2018-06-04 14:47:43 -0500

    new1

---
+++ orangecat.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var orangecat = express();
+
+orangecat.set("view-engine", "ejs")
+
+orangecat.get("/",function(req, res){
+	res.render("homepage")
+});
+
+orangecat.get("/about",function(req, res){
+	res.render("homepage")
+});
+
