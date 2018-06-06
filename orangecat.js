388708192d8691b194f027bc9997189d30f66a58
Ailisfather <yimingw@smu.edu>
2018-06-06 10:34:14 -0500

    html

--- orangecat.js
+++ orangecat.js
@@ -2,18 +2,18 @@ var express = require("express");
 var orangecat = express();
 const PORT = process.env.PORT || 5000
 
-orangecat.set("view engine", "ejs")
+//orangecat.set("view engine", "ejs")
 
 orangecat.get("/",function(req, res){
-	res.render("homepage")
+	res.render("homepage.ejs")
 });
 
 orangecat.get("/about",function(req, res){
-	res.render("about")
+	res.render("about.ejs")
 });
 
 orangecat.get("/1",function(req, res){
-	res.render("index")
+	res.render("index.html")
 });
 
 orangecat.listen(PORT, () => console.log(`Listening on ${ PORT }`));
