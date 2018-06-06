b95c573c7b91dd6e2d785f8bbabd392b30a51176
Ailisfather <yimingw@smu.edu>
2018-06-06 15:04:57 -0500

    chage

--- orangecat.js
+++ orangecat.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var orangecat = express();
 const PORT = process.env.PORT || 5000
-
+orangecat.use(express.static(path.join(__dirname, '/ressources')))
 //orangecat.set("view engine", "ejs")
 
 orangecat.get("/",function(req, res){
@@ -14,6 +14,22 @@ orangecat.get("/about",function(req, res){
 
 orangecat.get("/1",function(req, res){
 	res.render("index.ejs")
+});
+
+orangecat.get("/sketch.js",function(req, res){
+	res.render("sketch.js")
+});
+
+orangecat.get("/p5.js",function(req, res){
+	res.render("p5.js")
+});
+
+orangecat.get("/p5.dom.js",function(req, res){
+	res.render("p5.dom.js")
+});
+
+orangecat.get("/p5.sound.js",function(req, res){
+	res.render("p5.sound.js")
 });
 
 orangecat.listen(PORT, () => console.log(`Listening on ${ PORT }`));
