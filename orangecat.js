cffc56b0e20cc0b296087c468cbced17443bc080
Ailisfather <yimingw@smu.edu>
2018-07-07 18:08:39 -0500

    html

--- orangecat.js
+++ orangecat.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var orangecat = express();
 const PORT = process.env.PORT || 5000
-//orangecat.use(express.static(path.join(__dirname, '/ressources')))
+orangecat.use(express.static(path.join(__dirname, '/rcs')))
 //orangecat.set("view engine", "ejs")
 
 orangecat.get("/",function(req, res){
@@ -18,18 +18,6 @@ orangecat.get("/1",function(req, res){
 
 orangecat.get("/sketch.js",function(req, res){
 	res.render("sketch.js")
-});
-
-orangecat.get("/p5.js",function(req, res){
-	res.render("p5.js")
-});
-
-orangecat.get("/p5.dom.js",function(req, res){
-	res.render("p5.dom.js")
-});
-
-orangecat.get("/p5.sound.js",function(req, res){
-	res.render("p5.sound.js")
 });
 
 orangecat.listen(PORT, () => console.log(`Listening on ${ PORT }`));
