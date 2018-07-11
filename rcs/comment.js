9b4afe7a15c0bbce8e7ccff372dc321878807126
Ailisfather <yimingw@smu.edu>
2018-07-11 18:22:57 -0500

    html

--- rcs/comment.js
+++ rcs/comment.js
@@ -1,1 +1,17 @@
-var a = 1;
+var oText = document.getElementById('text');
+var oButton = document.getElementById('button');
+var oUl = document.getElementById('content');
+
+oButton.onclick = function(){
+	var sText = oText.value;
+	var oLi = document.createElement('li'); 
+	oLi.innerHTML = sText;
+	if (oUl.children.length > 0){
+	var oFirstNode = oUl.children[0];
+	oUl.insertBefore(oLi, oFirstNode);
+	}
+	else{
+	oUl.appendChild(oLi);
+	}
+
+}
